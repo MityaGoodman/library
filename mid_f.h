f60cced88484e62d35e38bcb63d102beaa2a5769
MityaGoodman <73709139+MityaGoodman@users.noreply.github.com>
2021-01-20 22:07:51 +0300

    Add files via upload

---
+++ mid_f.h
@@ -0,0 +1,37 @@
+#ifndef MID_F_H_INCLUDED
+#define MID_F_H_INCLUDED
+
+#include <iostream>
+
+using namespace std;
+
+void ft_num_print(int value);
+int ft_len_num(int value);
+int ft_sum_num(int value);
+int ft_multi_num(int value);
+int ft_abs(int value);
+bool check(int a);
+double ft_fabs(double value);
+double ft_fmin(double value1, double value2);
+double ft_fmax(double value1, double value2);
+int ft_max(int value1, int value2);
+int ft_min(int value1, int value2);
+double ft_scir(int radius);
+int ft_skv(int value);
+int ft_spr(int length, int width);
+int ft_sqrt(int value);
+bool isfPositive(double num);
+bool isPositive(int num);
+int power(int num, int exponent);
+void name();
+void fio();
+int chislo(int a);
+int ft_max_num(int value);
+int ft_min_num(int value);
+int ft_null_count(int value);
+bool ft_mirror_num(int value);
+int ft_mirror_count(int value);
+int ft_second_max(int value);
+int ft_second_simple_max_num(int value);
+
+#endif // MID_F_H_INCLUDED
